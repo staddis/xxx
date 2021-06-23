29dbc568511c23f512744a5fecc4edd4414452f8
swq <shengweiqiang@comac.intra>
2021-06-24 02:23:06 +0800

    1212

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -34,11 +34,11 @@ cron "10 * * * *" script-path=jd_dreamFactory.js,tag=京喜工厂
 
 const $ = new Env('京喜工厂');
 const JD_API_HOST = 'https://m.jingxi.com';
-const helpAu = true; //帮作者助力 免费拿活动
+const helpAu = false; //帮作者助力 免费拿活动
 const notify = $.isNode() ? require('./sendNotify') : '';
-let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
+let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = $.isNode() ? 20 : 5;
-let tuanActiveId = ``, hasSend = false;
+let tuanActiveId = `bozIUUFcANuUdWpw3QdvPw==`, hasSend = false;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const inviteCodes = [
@@ -48,7 +48,7 @@ const inviteCodes = [
 //   '-OvElMzqeyeGBWazWYjI1Q==',
 //   'GFwo6PntxDHH95ZRzZ5uAg=='
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-$.tuanIds = [];
+$.tuanIds = [fDG2FKL9Z5SW7kre5evcsQ==];
 $.appId = 10001;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
